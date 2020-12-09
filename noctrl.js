e0a37aabd7a9bf1c5461b6b78045f2e2f08e9ad2
Purple-Hacker <63299718+Purple-Hacker@users.noreply.github.com>
2020-12-09 14:37:44 +0100

    Update noctrl.js

--- noctrl.js
+++ noctrl.js
@@ -1,6 +1,24 @@
-while (true) {
-    if (e.ctrlKey) {
+/*while (true) {
+    /*if (e.ctrlKey) {
       console.log("ctr key was pressed");
       window.location.href = "https://google.com"; 
    }
-}
+}*/
+$(document).keydown(function(event) {
+    if (event.which == "17")
+        cntrlIsPressed = true;
+        console.log("ctr key was pressed");
+        window.location.href = "https://google.com"; 
+    else if (event.which == 65 && cntrlIsPressed) {
+        // Cntrl+  A
+        selectAllRows();
+        console.log("ctr + a key was pressed");
+        window.location.href = "https://google.com"; 
+    }
+});
+
+$(document).keyup(function() {
+    cntrlIsPressed = false;
+});
+
+var cntrlIsPressed = false;
